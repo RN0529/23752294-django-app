f1eab67aafb44585d7f8defa8f27a59589efd7f0
Richard <richardngowork@gmail.com>
2022-09-15 11:28:01 -0400

    fixed settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['https://serene-river-44710.herokuapp.com']
+ALLOWED_HOSTS = ['serene-river-44710.herokuapp.com']
